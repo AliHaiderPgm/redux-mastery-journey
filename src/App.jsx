75ce212339ae3919e52917c202b81cee74af564e
Ali Haider <alihaider203472@gmail.com>
2024-01-20 23:25:04 +0500

    feat(app): Basic Redux Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from './redux/slices/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+  const handleIncrement = () => {
+    dispatch(increment())
+  }
+  const handleDecrement = () => {
+    dispatch(decrement())
+  }
 
   return (
     <>
@@ -15,19 +23,20 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a href="https://redux.js.org/" target="_blank">
+          <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" className="logo redux" alt="React logo" />
+        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Redux</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>{count}</p>
+        <button onClick={handleIncrement}>
+          increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleDecrement}>
+          decrement
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
