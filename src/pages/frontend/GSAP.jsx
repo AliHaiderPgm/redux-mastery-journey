88f2ea8735587688bf321da83a7de8f27a9fd8ee
Ali Haider <alihaider203472@gmail.com>
2024-01-29 22:31:16 +0500

    feat(app): Minor Fix

--- src/pages/frontend/GSAP.jsx
+++ src/pages/frontend/GSAP.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import PreLoader from '../../components/PreLoader'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const GSAP = () => {
+    useGSAP(() => {
+        gsap.from('h1', { y: 70, delay: 1.9, opacity: 0, ease: 'circ' })
+    })
     return (
         <>
             <PreLoader />
-            <div className='d-flex justify-content-center align-items-center min-vh-100'>
-                <h1>GSAP</h1>
+            <div className='d-flex justify-content-center align-items-center min-vh-100 '>
+                <h1 className='display-1'>GSAP</h1>
             </div>
         </>
     )
