f7490d9ce38693b3444900971440d5dcdce4c0cd
Ali Haider <alihaider203472@gmail.com>
2024-02-02 23:27:21 +0500

    feat(app): Scroll Trigger Example 2

--- src/pages/frontend/Parallax.jsx
+++ src/pages/frontend/Parallax.jsx
@@ -1,20 +1,35 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import sky from "../../assets/sky.png"
+import mountains from "../../assets/mountains.png"
+import person from "../../assets/person.png"
 
 const Parallax = () => {
     gsap.registerPlugin(ScrollTrigger)
     useGSAP(() => {
-        gsap.to('.img', { y: 0, ease: 'none', scrollTrigger: { trigger: '.img', scrub: true } })
-        gsap.to('.text', { yPercent: -200, ease: 'none', scrollTrigger: { trigger: '.text', scrub: true } })
+        // Example 1
+        gsap.to('.img', { y: 10, ease: 'none', scrollTrigger: { trigger: '.img', scrub: true } })
+        gsap.to('.text', { y: -2500, ease: 'none', scrollTrigger: { trigger: '.text', scrub: 2, end: "bottom 90%", } })
+        // Example 2
+        const tl = gsap.timeline({ scrollTrigger: { scrub: true, } })
+        tl.to('.sky', { y: -700, ease: 'none', scrollTrigger: { trigger: '.sky', } })
+        tl.to('.mountain', { y: -400, ease: 'none', scrollTrigger: { trigger: '.mountain', } })
     })
     return (
         <div>
+            <h1>Example 1</h1>
             <div className="overflow-hidden position-relative">
                 <img src="https://source.unsplash.com/random/1920x1080" className="img-fluid img" alt="" />
-                <div className="position-absolute top-50 start-50 bg-white text-black p-4 rounded text">
+                <div className="position-absolute top-100 start-50 bg-white text-black p-4 rounded text">
                     <h1>One Day or Day One</h1>
                 </div>
+            </div>
+            <h1>Example 2</h1>
+            <div className="parallax">
+                <img src={sky} alt="Sky" className="img-fluid sky" />
+                <img src={mountains} alt="mountains" className="img-fluid mountain" />
+                <img src={person} alt="persons standing on mountain" className="img-fluid person" />
             </div>
             <p className="min-vh-100"></p>
         </div>
